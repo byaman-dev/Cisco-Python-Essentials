d9e6379b8ecc9730d34bf074042817d2fab9c014
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-05 05:26:34 +0530

    Code Documentation

---
+++ Module-2-Variables-DataTypes/sector_5_comments.py
@@ -0,0 +1,13 @@
+# --- SECTOR 5: Professional Documentation ---
+
+# Scenario 1: Purpose-driven Header
+"""
+PROJECT: Cisco Python Essentials
+MODULE: 02 - Data Fundamentals
+AUTHOR: @byaman-dev
+DESCRIPTION: Demonstrating the use of Docstrings for system documentation.
+"""
+
+# Scenario 2: Maintenance Tags
+# TODO: Refactor this section once Module 3 (Loops) is completed.
+print("Documentation Sector: Header and TODO tags added to source.")
