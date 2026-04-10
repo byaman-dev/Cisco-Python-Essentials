9f7d80f18e6d9360924adea59793f75034e7df1a
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-11 02:19:16 +0530

    Logic: Implementing a school-level math formula ($SI = \frac{P \times R \times T}{100}$).

---
+++ Module-2-Variables-DataTypes/Project and Practical's/02_simple_interest.py
@@ -0,0 +1,11 @@
+# File: 02_simple_interest.py
+# Objective: Financial calculation using user input.
+
+principal = float(input("Enter Principal amount: "))
+rate = float(input("Enter Annual Interest Rate (%): "))
+time = float(input("Enter Time (years): "))
+
+interest = (principal * rate * time) / 100
+
+print("The Simple Interest is:", interest)
+print("Total Amount to pay:", principal + interest)
