a1da76a8a1a0051be71eaa5b060bf1adabdb371e
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-11 02:17:54 +0530

    Logic: Basic variables and float math.

---
+++ Module-2-Variables-DataTypes/Project and Practical's/01_geometry_basic.py
@@ -0,0 +1,11 @@
+# File: 01_geometry_basic.py
+# Objective: Calculate the properties of a rectangle.
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area:", area)
+print("Perimeter:", perimeter)
