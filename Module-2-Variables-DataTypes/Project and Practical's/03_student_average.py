823d01bd6197b4133a46de56fb1045224059310d
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-11 02:22:23 +0530

    Calculate the average of 3 subjects

---
+++ Module-2-Variables-DataTypes/Project and Practical's/03_student_average.py
@@ -0,0 +1,10 @@
+# File: 03_student_average.py
+# Objective: Calculate the average of three subjects.
+
+math = float(input("Enter Math marks: "))
+physics = float(input("Enter Physics marks: "))
+chem = float(input("Enter Chemistry marks: "))
+
+average = (math + physics + chem) / 3
+
+print("Your Average Score is:", average)
