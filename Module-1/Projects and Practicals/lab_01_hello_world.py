dc0f7e459b3e67ed3f8a1a7786ea7b92901d0ae3
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-11 01:10:48 +0530

    lab_01_hello_world.py

---
+++ Module-1/Projects and Practicals/lab_01_hello_world.py
@@ -0,0 +1,7 @@
+# Cisco Python Essentials 1 - Module 1
+# Author: Aman
+# Goal: Test the environment and the print function.
+
+print("Hello, Python!")
+print("I am preparing for the MEXT scholarship.")
+print("Module 1 completed successfully.")
